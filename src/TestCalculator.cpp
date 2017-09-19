0d2e4b8ca81c7d1f4b2543e8cb61a8e61de831aa
Daniel Marcos <wisebdan@gmail.com>
2017-09-19 18:48:51 -0300

    Acrescenta arquivo TestExplanation

--- src/TestCalculator.cpp
+++ src/TestCalculator.cpp
@@ -52,9 +52,9 @@ TEST_CASE("Testes de sucesso:"){
           REQUIRE(StringCalculator("1,7\n\n\n\n\n\n\n\n\n\n,9\n"));
      }
      SECTION("3 numeros: Quebra de linha antes do segundo numero"){
-          REQUIRE(StringCalculator("1\n7,9\n"));
-          REQUIRE(StringCalculator("1\n\n7,9\n"));
-          REQUIRE(StringCalculator("1\n\n\n\n\n\n\n\n\n\n7,9\n"));
+          REQUIRE(StringCalculator("1,\n7,9\n"));
+          REQUIRE(StringCalculator("1,\n\n7,9\n"));
+          REQUIRE(StringCalculator("1,\n\n\n\n\n\n\n\n\n\n7,9\n"));
      }
      SECTION("3 numeros: Quebra de linha depois do primeiro numero"){
           REQUIRE(StringCalculator("1\n,7,9\n"));
@@ -76,30 +76,27 @@ TEST_CASE("Testes de sucesso:"){
 
      SECTION("Numero nulo em alguma posicao"){
           REQUIRE(StringCalculator("0\n"));
-          REQUIRE(StringCalculator("0,0\n"));
-          REQUIRE(StringCalculator("0,0,0\n"));
+          REQUIRE(StringCalculator("00,0\n"));
+          REQUIRE(StringCalculator("0,0000,0\n"));
 
           REQUIRE(StringCalculator("0,17\n"));
           REQUIRE(StringCalculator("17,0\n"));
-          REQUIRE(StringCalculator("0,8,9\n"));
-          REQUIRE(StringCalculator("8,0,9\n"));
+          REQUIRE(StringCalculator("0,008,9\n"));
+          REQUIRE(StringCalculator("17,0,0\n"));
      }
 
 
      SECTION("Mais que 3 numeros, mas em linhas diferentes"){
-          REQUIRE(StringCalculator("17\n"));
           REQUIRE(StringCalculator("1,2,3\n,4,5,6,\n7\n"));
      }
 
 
      SECTION("Numeros iguais"){
-          REQUIRE(StringCalculator("17\n"));
           REQUIRE(StringCalculator("9,9\n"));
           REQUIRE(StringCalculator("6,6,6\n"));
      }
 
      SECTION("Numeros decrescentes"){
-          REQUIRE(StringCalculator("17\n"));
           REQUIRE(StringCalculator("9,8\n"));
           REQUIRE(StringCalculator("9,7,1\n"));
      }
@@ -127,11 +124,11 @@ TEST_CASE("Testes de sucesso:"){
 TEST_CASE("Testes de fracasso:"){
      SECTION("Numero negativo"){
           REQUIRE(StringCalculator("-17\n"));
-          REQUIRE(StringCalculator("20,-10,7\n"));
+          REQUIRE(StringCalculator("20,-10,-7\n"));
      }
 
 
-     SECTION("1 numero"){
+     SECTION("Sem quebra de linha no final"){
           REQUIRE(StringCalculator("17"));
           REQUIRE(StringCalculator("\n17"));
      }
@@ -141,7 +138,8 @@ TEST_CASE("Testes de fracasso:"){
           REQUIRE(StringCalculator("17 \n"));
           REQUIRE(StringCalculator("17,\n"));
           REQUIRE(StringCalculator(",17\n"));
-          REQUIRE(StringCalculator("17"));
+          REQUIRE(StringCalculator("a17\n"));
+          REQUIRE(StringCalculator("8,a,9\n"));
      }
 
 
@@ -153,12 +151,12 @@ TEST_CASE("Testes de fracasso:"){
 
      SECTION("Delimitador"){
           REQUIRE(StringCalculator("//[,]8;9\n"));
-          REQUIRE(StringCalculator("//[,]1.7-9\n"));
+          REQUIRE(StringCalculator("//[,]1,7-9\n"));
 
-          REQUIRE(StringCalculator("/[;]17\n"));
-          REQUIRE(StringCalculator("//[;]1,7,9\n"));
+          REQUIRE(StringCalculator("//[;]8,9\n"));
+          REQUIRE(StringCalculator("//[;]1;7.9\n"));
 
-          REQUIRE(StringCalculator("//[-----]17\n"));
+          REQUIRE(StringCalculator("/[-----]17\n"));
           REQUIRE(StringCalculator("//[-----]1----7-----9\n"));
 
           REQUIRE(StringCalculator("//[!@#/]1!@#/7!@#9\n"));
