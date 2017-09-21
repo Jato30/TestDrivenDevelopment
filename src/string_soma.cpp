1268de9055d17f6f7a74df2a319ec371247a90cf
Daniel Marcos <wisebdan@gmail.com>
2017-09-21 00:40:38 -0300

    Refatora calculadora

--- src/string_soma.cpp
+++ src/string_soma.cpp
@@ -14,19 +14,32 @@ int string_soma(string string_entrada){
 
     // Lê a entrada
     for(uint index = 0; index < string_entrada.size(); index++){
-        
+
         if(isdigit(string_entrada[index])){
-        	int tenPower = -1; // Expoente da base 10 para multiplicar o número, transformando em centena, dezena, unidade, etc.
-        	for(uint i = index; isdigit(string_entrada[i]) != 0 && i < string_entrada.size(); i++){
-	  		tenPower++;
-        		lineDigits.push_back(atoi(&string_entrada[index]));
-        	}
-        	for(int i = tenPower; i >= 0; i--){
-        		finalSum += lineDigits[tenPower - i] * (int)pow(10.0, (float)tenPower);
-        	}
-        	
-        	index += tenPower;
-        	continue;
+            // int tenPower = -1; // Expoente da base 10 para multiplicar o número, transformando em centena, dezena, unidade, etc.
+            // for(uint i = index; isdigit(string_entrada[i]) != 0 && i < string_entrada.size(); i++){
+            //     tenPower++;
+            //     lineDigits.push_back(atoi(&string_entrada[index]));
+            // }
+            // for(int i = tenPower; i >= 0; i--){
+            //     finalSum += lineDigits[tenPower - i] * (int)pow(10.0, (float)i);
+            // }
+
+            // lineDigits.clear();
+            //index += tenPower;
+            if(atoi(&string_entrada[index]) > 0 && atoi(&string_entrada[index]) <= 1000){
+                finalSum += atoi(&string_entrada[index]);
+            }
+
+            int countDigits = 0;
+            int value = atoi(&string_entrada[index]);
+            while(value > 9){
+                countDigits++;
+                value /= 10;
+            }
+
+            index += countDigits;
+            continue;
         }
 
     }
